dc19a5c2a48687e21b0a884181edbdeb5a59dbed
Aleksa Dacic <aleksa.dacic99@gmail.com>
2022-05-14 18:51:05 +0200

    conflicts

--- nabavka/src/main/java/raf/si/racunovodstvo/nabavka/services/IKalkulacijaService.java
+++ nabavka/src/main/java/raf/si/racunovodstvo/nabavka/services/IKalkulacijaService.java
@@ -5,6 +5,9 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 import raf.si.racunovodstvo.nabavka.model.Kalkulacija;
 
+import java.util.List;
+import java.util.Map;
+
 public interface IKalkulacijaService extends IService<Kalkulacija, Long> {
 
     Page<Kalkulacija> findAll(Specification<Kalkulacija> spec, Pageable pageSort);
