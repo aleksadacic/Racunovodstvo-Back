68a6eb3cb71023b5356e3cb21c6d4d96ae904502
Aleksa Dacic <aleksa.dacic99@gmail.com>
2022-06-03 18:30:58 +0200

    Ispravke za komentare od @borisstojanovic

--- preduzece/src/main/java/raf/si/racunovodstvo/preduzece/jobs/ObracunZaradeJob.java
+++ preduzece/src/main/java/raf/si/racunovodstvo/preduzece/jobs/ObracunZaradeJob.java
@@ -1,11 +1,11 @@
 package raf.si.racunovodstvo.preduzece.jobs;
 
 import lombok.Getter;
-import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import raf.si.racunovodstvo.preduzece.services.impl.ObracunZaradeService;
 
+import java.time.DateTimeException;
 import java.time.ZonedDateTime;
 import java.time.temporal.ChronoUnit;
 import java.util.Date;
@@ -15,13 +15,20 @@ import java.util.concurrent.TimeUnit;
 
 @Service
 public class ObracunZaradeJob {
-    private final ObracunZaradeService obracunZaradeService;
+    final ObracunZaradeService obracunZaradeService;
+    private ZonedDateTime nextDate;
 
     /**
      * dayOfMonth from 1 to 31
      */
-    @Getter @Setter
+    @Getter
     private int dayOfMonth = JobConstants.DEFAULT_DAY_OF_MONTH;
+
+    public void setDayOfMonth(int dayOfMonth) throws DateTimeException {
+        ZonedDateTime.of(nextDate.getMonthValue(), dayOfMonth,
+                0, 0, 0, 0, 0, nextDate.getZone());
+        this.dayOfMonth = dayOfMonth;
+    }
 
     @Autowired
     public ObracunZaradeJob(ObracunZaradeService obracunZaradeService) {
@@ -35,10 +42,10 @@ public class ObracunZaradeJob {
             @Override
             public void run() {
                 ZonedDateTime now = ZonedDateTime.now();
-                long delay = now.until(now.plusMonths(1), ChronoUnit.MILLIS);
+                nextDate = now.plusMonths(1).withDayOfMonth(dayOfMonth);
+                long delay = now.until(nextDate, ChronoUnit.MILLIS);
 
                 try {
-                    // Provera sa specifikacijom mozda treba now.minusMonths(1) ako je potrebno za prosli mesec
                     obracunZaradeService.makeObracunZarade(Date.from(now.toInstant()));
                 } finally {
                     executor.schedule(this, delay, TimeUnit.MILLISECONDS);
